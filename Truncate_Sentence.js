a077a81853d7c73f2ad9835a0e095d90494e0d4a
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2024-07-18 18:47:45 +0530

    Add files via upload

---
+++ Truncate_Sentence.js
@@ -0,0 +1,10 @@
+var truncateSentence = function(s, k) {
+   const result=[]
+   for(let i=0;i<s.lemgth;i++){
+    const word=s[i];
+    result.push(s)
+   }
+   return result;
+};
+truncateSentence("hi how are you")
+truncateSentence(5)
